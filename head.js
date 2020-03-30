95ccaff49d6bfe16af5970e06e25c9bb4b17b5f2
Jessie Wright <jessie.wright@gmail.com>
2020-03-30 22:05:38 +0000

    implemented head

---
+++ head.js
@@ -0,0 +1,19 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✔️ ✔️ ✔️  Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`✖️ ✖️ ✖️  Assertion Failed: ${actual} !== ${expected}`);
+  }
+
+};
+
+
+
+
+const head = function(arry){
+  return arry[0];
+}
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([], undefined));
