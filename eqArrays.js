1aa0aa78fb1af9e0c2ad96d5967d1b1322a77dfa
Jessie Wright <jessie.wright@gmail.com>
2020-03-31 20:58:01 +0000

    initial implementation of eqArrays

---
+++ eqArrays.js
@@ -0,0 +1,27 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✔️ ✔️ ✔️  Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`✖️ ✖️ ✖️  Assertion Failed: ${actual} !== ${expected}`);
+  }
+
+};
+
+
+const eqArrays = function(actual, expected) {
+  if(actual.length !== expected.length) {
+    return false;
+  }
+  for (let i = 0; i < actual.length; i++) {
+    if (actual[i] !== expected[i]) {
+      return false
+    }
+  }
+  return true;
+};
+
+
+assertEqual(eqArrays([1,2,3], [1,2,3]), true);
+assertEqual(eqArrays([1,2], [1,2,3]), false);
+assertEqual(eqArrays([1,2,3], [1,2,4]), false);
