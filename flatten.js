f4ec3246dd564be5bd400a93c1befad351c97b70
Jessie Wright <jessie.wright@gmail.com>
2020-03-31 22:03:26 +0000

    initial version of flatten

---
+++ flatten.js
@@ -0,0 +1,38 @@
+const eqArrays = function(actual, expected) {
+  if(actual.length !== expected.length) {
+    return false;
+  }
+  for (let i = 0; i < actual.length; i++) {
+    if (actual[i] !== expected[i]) {
+      return false
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function (actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✔️ ✔️ ✔️  Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`✖️ ✖️ ✖️  Assertion Failed: ${actual} !== ${expected}`);
+  }
+}
+
+
+const flatten = function(masterArray) {
+  let result = [];
+  for (let elem of masterArray) {
+    if(Array.isArray(elem)) {
+      for (let item of elem) {
+        result.push(item);
+      }
+    } else {
+      result.push(elem);
+    }
+  }
+  return result;
+}
+
+
+assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6]);
+
