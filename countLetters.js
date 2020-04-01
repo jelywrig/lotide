2af4d542a1235b6ab8b2b062a634347c9d93ecd0
Jessie Wright <jessie.wright@gmail.com>
2020-04-01 19:37:46 +0000

    implemented countLetters

---
+++ countLetters.js
@@ -0,0 +1,26 @@
+const countLetters = function(string) {
+  let noSpacesStr = string.replace(/\s/g, '');
+  let result = {};
+
+  for (let char of noSpacesStr) {
+    if(result[char]){
+      result[char] += 1;
+    } else {
+      result[char] = 1;
+    }
+  }
+
+  return result;
+
+}
+
+
+
+
+
+
+
+
+
+
+console.log(countLetters("hello i am a string"));
