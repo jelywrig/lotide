98d8ff10721c46145eec74997cc6e2a4d16ab473
Jessie Wright <jessie.wright@gmail.com>
2020-04-01 20:48:33 +0000

    added array comparison

--- eqObjects.js
+++ eqObjects.js
@@ -1,11 +1,16 @@
 const assertEqual = require('./assertEqual');
+const eqArrays = require('./eqArrays');
 
 const eqObjects = function(object1, object2) {
   if(Object.keys(object1).length !== Object.keys(object2).length) {
     return false;
   }
   for (let key in object1) {
-    if(object1[key] !== object2[key]) {
+    if (Array.isArray(object1[key]) && Array.isArray(object2[key])){
+      if (!eqArrays(object1[key], object2[key])) {
+        return false;
+      }
+    }else if (object1[key] !== object2[key]) {
       return false;
     }
   }
