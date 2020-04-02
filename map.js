fb2cd184db1f20a84fff3ff208fd11fed3cc8986
Jessie Wright <jessie.wright@gmail.com>
2020-04-02 20:03:42 +0000

    implemented map with some tests

---
+++ map.js
@@ -0,0 +1,16 @@
+const map = function(array, callback) {
+  const results =[];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+
+  return results;
+}
+
+const assertArraysEqual = require('./assertArraysEqual');
+const words = ["ground", "control", "to", "major", "tom"];
+
+assertArraysEqual(map(words, word => word + 's'), [ 'grounds', 'controls', 'tos', 'majors', 'toms' ]);
+assertArraysEqual(map(words, word => word.length), [ 6, 7, 2, 5, 3 ]);
+
+assertArraysEqual(map('hello', x => x.toUpperCase()), [ 'H', 'E', 'L', 'L', 'O' ]);
