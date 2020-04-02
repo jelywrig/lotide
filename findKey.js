643395a3ef4515aed81f47317a2a8711fe62d3f4
Jessie Wright <jessie.wright@gmail.com>
2020-04-02 20:36:54 +0000

    implemented findKey

---
+++ findKey.js
@@ -0,0 +1,29 @@
+const findKey = function (obj, callback) {
+  for (let key in obj) {
+    if (callback(obj[key])) { 
+      return key;
+    }
+  }
+// if val not found function will return undefined automatically
+}
+
+
+assertEqual = require('./assertEqual');
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2), "noma") // => "noma")
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 4), undefined) // => "noma")
