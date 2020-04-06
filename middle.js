1e3ad33223559a9daed17815efa7cf17cb6dbd05
Jessie Wright <jessie.wright@gmail.com>
2020-04-06 16:10:24 +0000

    refactored to separate tests and export/require

--- middle.js
+++ middle.js
@@ -1,5 +1,3 @@
-const assertArraysEqual = require('./assertArraysEqual');
-
 const middle = function(arry) {
   let result = [];
   if (arry.length > 2) {
@@ -14,13 +12,4 @@ const middle = function(arry) {
   return result;
 }
 
-
-if(require.main === module) {
-  assertArraysEqual(middle([1]),[]);
-  assertArraysEqual(middle([1,2]), []);
-  assertArraysEqual(middle([1,2,3]), [2]);
-  assertArraysEqual(middle([1,2,3,4,5]), [3]);
-  assertArraysEqual(middle([1,2,3,4]), [2,3]);
-  assertArraysEqual(middle([1,2,3,4,5,6]), [3,4]);
-
-}
+module.exports = middle;
