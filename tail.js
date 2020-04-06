6f169ea8fc4ea6d28b239e4de431818a2290a6ed
Jessie Wright <jessie.wright@gmail.com>
2020-04-06 15:43:10 +0000

    refactored to export/require and have seperate test file

--- tail.js
+++ tail.js
@@ -1,24 +1,5 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✔️ ✔️ ✔️  Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`✖️ ✖️ ✖️  Assertion Failed: ${actual} !== ${expected}`);
-  }
-
-};
-
 const tail = function(arry) {
   return arry.slice(1);
 };
 
-// Test Case 1: Check the returned array elements
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
-
-//Test Case 2: Make sure original array isn't altered
-
-let testArry = [1,2,3];
-let result2 = tail(testArry);
-assertEqual(testArry[0],1);
+module.exports = tail;
